f5187567452d23dcbf37e63d80b5b0df5c3997bd
Nabeel <nabeelmhd11@gmail.com>
2022-09-01 20:34:49 +0530

    initail commit along with a mvc application and an api project

---
+++ testApi/testApi/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Http;
+using testApi.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testApi.Controllers
+{
+    [System.Web.Http.Route("api/test")]
+    public class HomeController : ApiController
+    {
+        private readonly ITestService _testService;
+        public HomeController(ITestService testService)
+        {
+            this._testService = testService;
+        }
+        public IActionResult Index()
+        {
+            throw new Exception();
+        }
+
+        [System.Web.Http.Route("getData/{id}")]
+        [System.Web.Http.HttpGet]
+        public IActionResult getData(int id)
+        {
+            var isExist =  this._testService.getData(id);
+            return (IActionResult)this.Ok<object>(isExist);
+        }
+    }
+}
---
+++ testApi/testApi/Services/TestService.cs
@@ -0,0 +1,34 @@
+﻿
+using testApi.Interface;
+
+namespace testApi.Services
+{
+    /// <summary>
+    /// TestService
+    /// </summary>
+    /// <seealso cref="testApi.Interface.ITestService" />
+    public class TestService : ITestService
+    {
+        /// <summary>
+        /// Gets the data.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        public bool getData(int id)
+        {
+            var data = new
+            {
+                id = 1,
+                name = "nabeel"
+            };
+            if(data.id == id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
